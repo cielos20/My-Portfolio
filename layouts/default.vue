4484aba86830b2f084d8e4cf4073a9f9fd0b79cc
cielos20 <60800119+cielos20@users.noreply.github.com>
2021-07-13 02:06:06 +0100

    Finished the about section
    Just need to replace with a photo of me

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-app-bar app>
-        <v-app-bar-title class="ml-lg-16  text-h7">Rodrigo</v-app-bar-title>
+        <v-app-bar-title class="ml-lg-16 text-sm">Rodrigo</v-app-bar-title>
         <v-spacer></v-spacer>
         <v-btn text>About</v-btn>
-        <v-btn text>Portfolio</v-btn>
         <v-btn text>Skills</v-btn>
+        <v-btn text>Portfolio</v-btn>
         <v-btn text>Contacts</v-btn>
         <v-btn text>Blog</v-btn>
     </v-app-bar>
